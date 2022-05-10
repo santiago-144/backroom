ac659a9111be1c4a5f1d25d4f3ba02549e2dd3f2
Gabriel G <gayagocr@gmail.com>
2022-05-10 12:36:19 -0500

    fet(boostrap config/working in registro page)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,5 +2,5 @@ import '../main.css'
 
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <a href="../pages/Registro.html" target="_blank">Documentation</a>
 `
