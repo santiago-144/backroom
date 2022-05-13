e5b47739007167b8e238b10728b7beb96bcf604f
Angel-LH <angellopez06@gmail.com>
2022-05-13 10:53:12 -0500

    Pagina de contacto y esqueleto del la pagina de Perfil

---
+++ src/scripts/main.js
@@ -0,0 +1,7 @@
+import '../main.css'
+
+document.querySelector('#app').innerHTML = `
+  <h1>Backroom</h1>
+  <a href="../pages/Registro.html" target="_blank">Registrate</a>
+  <a href="../pages/Login.html">Iniciar sesion</a>
+`
