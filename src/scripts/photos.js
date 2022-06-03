bddb4f15589c888e2c2d32761c75ef19001e7c19
sesaicazares <sesaiczrs@gmail.com>
2022-06-03 16:49:44 -0500

    review

--- src/scripts/photos.js
+++
@@ -1,9 +0,0 @@
-const fullImgBox = document.getElementById("fullImgBox");
-fullImg = document.getElementById("fullImg");
-
-function openFullImg(refer){
-    fullImgBox.style.display = "flex"
-}
-function closeImg(){
-    fullImgBoxstyle.display = "nome";
-}
